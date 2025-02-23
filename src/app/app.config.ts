a15d829f3ca8613174192b3aecffe03dfd3b8ae0
Mariomedhat899 <mariomedhat899@gmail.com>
2025-02-24 00:34:41 +0200

    configcommit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
@@ -11,7 +11,7 @@ import { loadingInterceptor } from './core/interceptor/loading...interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true })
-    , provideRouter(routes),
+    , provideRouter(routes, withHashLocation()),
      provideClientHydration(withEventReplay()),
     provideHttpClient(withFetch(),withInterceptors([authInterceptor,loadingInterceptor])),
   importProvidersFrom(BrowserAnimationsModule,NgxSpinnerModule),
